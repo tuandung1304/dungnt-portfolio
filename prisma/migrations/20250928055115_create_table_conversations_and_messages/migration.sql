82639dfd480638cea24dbc6317380a7425237cae
tuandung1304 <tuandung13401@gmail.com>
2025-09-28 13:21:25 +0700

    Install prisma and create table conversations, messages

---
+++ prisma/migrations/20250928055115_create_table_conversations_and_messages/migration.sql
@@ -0,0 +1,26 @@
+-- CreateEnum
+CREATE TYPE "public"."MessageRole" AS ENUM ('user', 'assistant', 'system');
+
+-- CreateTable
+CREATE TABLE "public"."conversations" (
+    "id" TEXT NOT NULL,
+    "title" TEXT,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "conversations_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "public"."messages" (
+    "id" TEXT NOT NULL,
+    "text" TEXT NOT NULL,
+    "role" "public"."MessageRole" NOT NULL,
+    "conversationId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "messages_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "public"."messages" ADD CONSTRAINT "messages_conversationId_fkey" FOREIGN KEY ("conversationId") REFERENCES "public"."conversations"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
